d05b0d265112c5702a4b0f48adc02079d73eb8eb
maurogimenezb <maurogimenezb@gmail.com>
2023-11-10 16:19:13 -0300

    segundo commit 10/11

--- lesTourApp/tests.py
+++ lesTourApp/tests.py
@@ -1,3 +1,71 @@
 from django.test import TestCase
+from .models import Ciudades, Hoteles, Areas, Cargo, Empleados, Clientes, Tipo_Habitacion, Habitacion, Reservas, Reserva_Huesped
 
-# Create your tests here.
+class ModelTests(TestCase):
+    def setUp(self):
+        ciudad = Ciudades.objects.create(nombre='Asuncion')
+        area = Areas.objects.create(nombre='Recepción')
+        cargo = Cargo.objects.create(nombre='Recepcionista', id_area=area)
+        hotel = Hoteles.objects.create(
+            nombre='Hotel iturbe',
+            ciudad=ciudad,
+            barrio='san juan',
+            direccion='iturbe',
+            telefono=123456789,
+            email='hola@hotel.com',
+            pisos=3,
+            habitaciones=50
+        )
+        empleado = Empleados.objects.create(
+            ci_numero=1234567,
+            nombre='Juan Gonzalez',
+            email='juan@hotel.com',
+            direccion='villarrica',
+            ciudad=ciudad,
+            telefono=987654321,
+            cargo=cargo,
+            hotel=hotel
+        )
+        cliente = Clientes.objects.create(
+            ci_numero=9876543,
+            nombre='Mauro Gimenez',
+            email='mauro@hola.com',
+            direccion='Iturbe',
+            telefono=555666777
+        )
+        tipo_habitacion = Tipo_Habitacion.objects.create(
+            nombre='Suite',
+            capacidad=2,
+            costo=150
+        )
+        habitacion = Habitacion.objects.create(
+            numero=101,
+            piso=1,
+            id_tipo_habitacion=tipo_habitacion,
+            id_hotel=hotel
+        )
+        reserva = Reservas.objects.create(
+            id_cliente=cliente,
+            fecha_entrada='2023-11-10 12:00:00',
+            fecha_salida='2023-11-12 12:00:00',
+            precio=200,
+            numero_personas=2,
+            estado='Activa',
+            id_habitacion=habitacion
+        )
+        Reserva_Huesped.objects.create(
+            id_cliente=cliente,
+            id_reserva=reserva
+        )
+
+    def test_hoteles_model(self):
+        hotel = Hoteles.objects.get(nombre='Hotel Iturbe')
+
+        self.assertEqual(hotel.ciudad.nombre, 'Asuncion')
+        self.assertEqual(hotel.barrio, 'San Juan')
+        self.assertEqual(hotel.direccion, 'Iturbe')
+        self.assertEqual(hotel.telefono, 123456789)
+        self.assertEqual(hotel.email, 'iturbe@hotel.com')
+        self.assertEqual(hotel.pisos, 3)
+        self.assertEqual(hotel.habitaciones, 50)
+
--- lesTourWeb/tests.py
+++ lesTourWeb/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from lesTourWeb.views import home, signUp, reservation, signOut, signIn, Hoteles, Personal, Usuarios
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home)
+
+    def test_signup_url_resolves(self):
+        url = reverse('signup')
+        self.assertEqual(resolve(url).func, signUp)
+
+    def test_reservation_url_resolves(self):
+        url = reverse('reservation')
+        self.assertEqual(resolve(url).func, reservation)
+
+    def test_signout_url_resolves(self):
+        url = reverse('logout')
+        self.assertEqual(resolve(url).func, signOut)
+
+    def test_signin_url_resolves(self):
+        url = reverse('signin')
+        self.assertEqual(resolve(url).func, signIn)
+
+    def test_hoteles_url_resolves(self):
+        url = reverse('hoteles')
+        self.assertEqual(resolve(url).func, Hoteles)
+
+    def test_personal_url_resolves(self):
+        url = reverse('personal')
+        self.assertEqual(resolve(url).func, Personal)
+
+    def test_usuarios_url_resolves(self):
+        url = reverse('usuarios')
+        self.assertEqual(resolve(url).func, Usuarios)
+
+class TestViews(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_signup_view(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_reservation_view(self):
+        response = self.client.get(reverse('reservation'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_signout_view(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_signin_view(self):
+        response = self.client.get(reverse('signin'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_hoteles_view(self):
+        response = self.client.get(reverse('hoteles'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_personal_view(self):
+        response = self.client.get(reverse('personal'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
+
+    def test_usuarios_view(self):
+        response = self.client.get(reverse('usuarios'))
+        self.assertEqual(response.status_code, 200)
+        # Añade más aserciones según sea necesario
